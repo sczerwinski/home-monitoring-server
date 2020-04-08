76c107f746a7c32685b3633da5bfc135c4130235
Slawomir Czerwinski <slawomir@czerwinski.it>
2020-04-08 20:16:49 +0100

    Logging important events

--- src/main/kotlin/model/LocationRepositoryImpl.kt
+++ src/main/kotlin/model/LocationRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package it.czerwinski.home.monitoring.model
 
 import it.czerwinski.home.monitoring.db.LocationDao
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -8,13 +9,25 @@ class LocationRepositoryImpl(
     private val locationDao: LocationDao
 ) : LocationRepository {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override fun findAll(): List<Location> =
         locationDao.findAll().toList()
 
     override fun findByName(name: String): Location? =
-        locationDao.findByName(name).orElse(null)
+        locationDao.findByName(name)
+            .orElseGet {
+                logger.warn("Location \"$name\" doesn't exist")
+                null
+            }
 
     override fun findByNameOrCreate(name: String): Location =
         locationDao.findByName(name)
-            .orElseGet { locationDao.save(Location(name = name)) }
+            .orElseGet {
+                logger.warn("Location \"$name\" doesn't exist")
+                locationDao.save(Location(name = name))
+                    .also { savedLocation ->
+                        logger.info("Location created: $savedLocation")
+                    }
+            }
 }
--- src/main/kotlin/model/SensorReadingRepositoryImpl.kt
+++ src/main/kotlin/model/SensorReadingRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package it.czerwinski.home.monitoring.model
 
 import it.czerwinski.home.monitoring.db.SensorReadingDao
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 import java.time.LocalDate
 import java.time.Period
@@ -9,6 +10,8 @@ import java.time.Period
 class SensorReadingRepositoryImpl(
     private val sensorReadingDao: SensorReadingDao
 ) : SensorReadingRepository {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     override fun findLatestReading(location: Location, type: SensorType): SensorReading? =
         sensorReadingDao.findFirst1ByLocationAndTypeOrderByDateTimeDesc(
@@ -25,6 +28,7 @@ class SensorReadingRepositoryImpl(
         ).toList()
 
     override fun save(sensorReading: SensorReading) {
-        sensorReadingDao.save(sensorReading)
+        val savedReading = sensorReadingDao.save(sensorReading)
+        logger.info("Sensor reading created: $savedReading")
     }
 }
